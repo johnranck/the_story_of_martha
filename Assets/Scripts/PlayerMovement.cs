4983c6a9f32f8e2d0423fcbfeb6002acb38fccf5
John Ranck <jranck333@gmail.com>
2020-07-15 10:53:04 -0500

    new2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,7 +19,9 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
-    private Animator animator; 
+    private Animator animator;
+    public FloatValue currentHealth;
+    public Signal playerHealthSignal; 
 
     // Start is called before the first frame update
     void Start()
@@ -83,9 +85,14 @@ public class PlayerMovement : MonoBehaviour
             );
     }
 
-    public void Knock(float knockTime)
+    public void Knock(float knockTime, float damage)
     {
-        StartCoroutine(KnockCo(knockTime));
+        currentHealth.initialValue -= damage;
+        if (currentHealth.initialValue > 0)
+        {
+            playerHealthSignal.Raise(); 
+            StartCoroutine(KnockCo(knockTime));
+        }
     }
 
     private IEnumerator KnockCo(float knockTime)
